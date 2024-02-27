9da189a63f6047ec67ed03531984db3688218e0b
desmondr18 <desmondrj18@gmail.com>
2024-02-26 21:11:40 -0700

    Se agregaron nuevas funciones a php para las bases de datos

--- cuidadores.php
+++ cuidadores.php
@@ -1,19 +1,19 @@
 <?php
 
-// Reemplazar con las credenciales de tu base de datos
 $host = "localhost";
 $puerto = "5432";
-$usuario = "usuario";
-$contrasena = "contrasena";
-$base_datos = "nombre_base_datos";
+$usuario = "postgres";
+$contrasena = "postgres";
+$base_datos = "proyectodb";
 
 // Conexión a la base de datos
 $conexion = pg_connect("host=$host port=$puerto dbname=$base_datos user=$usuario password=$contrasena");
+try {
+    $conn = new PDO("pgsql:host=$host;port=$puerto;dbname=$base_datos;user=$usuario;password=$contrasena");
+    echo "Conexión exitosa!";
+  } catch (PDOException $e) {
+    echo "Error al conectar: " . $e->getMessage();
+  }
 
-// Verificar si la conexión fue exitosa
-if (!$conexion) {
-    echo "Error al conectar a la base de datos: " . pg_last_error($conexion);
-    exit();
-}
 
 ?>
